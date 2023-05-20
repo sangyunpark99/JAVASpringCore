b9a8523e6698eaf6728f55f4c989fb67898fe3fb
sangyunpark99 <psangyun5108@gmail.com>
2023-05-20 11:21:51 +0900

    중복된 빈이 자동주입되는 경우와 수동주입과 자동주입이 되는 경우

--- src/main/java/sangyunpark/core/AutoAppConfig.java
+++ src/main/java/sangyunpark/core/AutoAppConfig.java
@@ -1,15 +1,20 @@
 package sangyunpark.core;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import sangyunpark.core.member.MemoryMemberRepository;
 
 @Configuration
 @ComponentScan( // 제외할 부분을 지정
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
-
+    @Bean(name="memoryMemberRepository")
+    MemoryMemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
 }
 
 
