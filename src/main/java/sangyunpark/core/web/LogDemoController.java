35dcb8adc561d1a60d68d6a5bf783910b8fc7f45
sangyunpark99 <psangyun5108@gmail.com>
2023-05-24 09:37:21 +0900

    스코프와 프록시

---
+++ src/main/java/sangyunpark/core/web/LogDemoController.java
@@ -0,0 +1,32 @@
+package sangyunpark.core.web;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import sangyunpark.core.common.MyLogger;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+       String requestURL = request.getRequestURL().toString();
+
+        System.out.println("myLogger = " + myLogger.getClass());
+
+       myLogger.setRequestURL(requestURL);
+
+       myLogger.log("controller test");
+       logDemoService.logic("testId");
+
+       return "Ok";
+    }
+}
